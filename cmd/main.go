e9fbb68170b6def9dc454a9edc42e88fb2e2f93e
Zetni <azakharov@workmovr.com>
2024-12-22 20:55:18 +0300

    feat: main

---
+++ cmd/main.go
@@ -0,0 +1,9 @@
+package main 
+
+import (
+	"github.com/MCtop4ik/go-first-sprint/internal/application"
+)
+
+func main() {
+	application.RunServer()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/MCtop4ik/go-first-sprint
+
+go 1.23.0
