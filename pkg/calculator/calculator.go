898e241a99633336f2d576fe02a43965320f5b2d
Zetni <azakharov@workmovr.com>
2024-12-23 14:08:37 +0300

    feat: add fractions

--- pkg/calculator/calculator.go
+++ pkg/calculator/calculator.go
@@ -7,19 +7,9 @@ import (
 )
 
 func stringToFloat64(str string) float64 {
-	degree := float64(1)
-	var res float64 = 0
-	var invers bool = false
-	for i := len(str); i > 0; i-- {
-		if str[i-1] == '-' {
-			invers = true
-		} else {
-			res += float64(9-int('9'-str[i-1])) * degree
-			degree *= 10
-		}
-	}
-	if invers {
-		res = 0 - res
+	res, err := strconv.ParseFloat(str, 64)
+	if err != nil {
+		return 0
 	}
 	return res
 }
@@ -30,25 +20,26 @@ func isSign(value rune) bool {
 
 func Calc(expression string) (float64, error) {
 	if len(expression) < 3 {
-		return 0, fmt.Errorf("???")
+		return 0, fmt.Errorf("Недостаточно символов для вычисления")
 	}
-	//////////////////////////////////////////////////////////////////////////////////////////////////////
+
 	var res float64
 	var b string
 	var c rune = 0
 	var resflag bool = false
 	var isc int
 	var countc int = 0
-	//////////////////////////////////////////////////////////////////////////////////////////////////////
+
 	for _, value := range expression {
 		if isSign(value) {
 			countc++
 		}
 	}
-	//////////////////////////////////////////////////////////////////////////////////////////////////////
+
 	if isSign(rune(expression[0])) || isSign(rune(expression[len(expression)-1])) {
-		return 0, fmt.Errorf("???")
+		return 0, fmt.Errorf("Некорректное выражение")
 	}
+
 	for i, value := range expression {
 		if value == '(' {
 			isc = i
@@ -56,19 +47,19 @@ func Calc(expression string) (float64, error) {
 		if value == ')' {
 			calc, err := Calc(expression[isc+1 : i])
 			if err != nil {
-				return 0, fmt.Errorf("???")
+				return 0, fmt.Errorf("Некорректное выражение")
 			}
-			calcstr := strconv.FormatFloat(calc, 'f', 0, 64)
+			calcstr := strconv.FormatFloat(calc, 'f', -1, 64)
 			i2 := i
 			i -= len(expression[isc:i+1]) - len(calcstr)
-			expression = strings.Replace(expression, expression[isc:i2+1], calcstr, 1) // Меняем скобки на результат выражения в них
+			expression = strings.Replace(expression, expression[isc:i2+1], calcstr, 1)
 		}
 	}
+
 	if countc > 1 {
 		for i := 1; i < len(expression); i++ {
 			value := rune(expression[i])
-			///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-			//Умножение и деление
+
 			if value == '*' || value == '/' {
 				var imin int = i - 1
 				if imin != 0 {
@@ -90,23 +81,23 @@ func Calc(expression string) (float64, error) {
 				}
 				calc, err := Calc(expression[imin:imax])
 				if err != nil {
-					return 0, fmt.Errorf("???")
+					return 0, fmt.Errorf("Некорректное выражение")
 				}
-				calcstr := strconv.FormatFloat(calc, 'f', 0, 64)
+				calcstr := strconv.FormatFloat(calc, 'f', -1, 64)
 				i -= len(expression[isc:i+1]) - len(calcstr) - 1
-				expression = strings.Replace(expression, expression[imin:imax], calcstr, 1) // Меняем скобки на результат выражения в них
+				expression = strings.Replace(expression, expression[imin:imax], calcstr, 1)
 			}
 			if value == '+' || value == '-' || value == '*' || value == '/' {
 				c = value
 			}
 		}
 	}
-	//////////////////////////////////////////////////////////////////////////////////////////////////////
+
 	for _, value := range expression + "s" {
 		switch {
 		case value == ' ':
 			continue
-		case value > 47 && value < 58: // Если это цифра
+		case (value >= '0' && value <= '9') || value == '.': // Если это цифра или точка
 			b += string(value)
 		case isSign(value) || value == 's': // Если это знак
 			if resflag {
@@ -131,10 +122,9 @@ func Calc(expression string) (float64, error) {
 			b = strings.ReplaceAll(b, b, "")
 			c = value
 
-			/////////////////////////////////////////////////////////////////////////////////////////////
 		case value == 's':
 		default:
-			return 0, fmt.Errorf("Not correct input")
+			return 0, fmt.Errorf("Некорректный ввод")
 		}
 	}
 	return res, nil
